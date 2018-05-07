f0679fdd7901bac6529e3e316105d62741797c19
jmartingimenez <jmartingimenez@hotmail.com>
2018-05-07 02:11:25 -0300

    TabIndex reorganizado en el Form de Menú

--- Math Challenge/Math Challenge/Forms/MenuForm.Designer.cs
+++ Math Challenge/Math Challenge/Forms/MenuForm.Designer.cs
@@ -40,10 +40,10 @@
             // 
             // Sumar
             // 
-            this.Sumar.Location = new System.Drawing.Point(154, 62);
+            this.Sumar.Location = new System.Drawing.Point(21, 103);
             this.Sumar.Name = "Sumar";
             this.Sumar.Size = new System.Drawing.Size(115, 35);
-            this.Sumar.TabIndex = 0;
+            this.Sumar.TabIndex = 1;
             this.Sumar.Text = "Sumar";
             this.Sumar.UseVisualStyleBackColor = true;
             this.Sumar.Click += new System.EventHandler(this.Sumar_Click);
@@ -53,17 +53,17 @@
             this.Restar.Location = new System.Drawing.Point(154, 103);
             this.Restar.Name = "Restar";
             this.Restar.Size = new System.Drawing.Size(115, 35);
-            this.Restar.TabIndex = 1;
+            this.Restar.TabIndex = 2;
             this.Restar.Text = "Restar";
             this.Restar.UseVisualStyleBackColor = true;
             this.Restar.Click += new System.EventHandler(this.Restar_Click);
             // 
             // Mixto
             // 
-            this.Mixto.Location = new System.Drawing.Point(154, 192);
+            this.Mixto.Location = new System.Drawing.Point(154, 144);
             this.Mixto.Name = "Mixto";
-            this.Mixto.Size = new System.Drawing.Size(115, 35);
-            this.Mixto.TabIndex = 2;
+            this.Mixto.Size = new System.Drawing.Size(115, 37);
+            this.Mixto.TabIndex = 4;
             this.Mixto.Text = "Mixto";
             this.Mixto.UseVisualStyleBackColor = true;
             this.Mixto.Click += new System.EventHandler(this.Mixto_Click);
@@ -72,10 +72,10 @@
             // 
             this.Records.BackColor = System.Drawing.Color.PapayaWhip;
             this.Records.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Records.Location = new System.Drawing.Point(21, 149);
+            this.Records.Location = new System.Drawing.Point(21, 192);
             this.Records.Name = "Records";
-            this.Records.Size = new System.Drawing.Size(115, 78);
-            this.Records.TabIndex = 3;
+            this.Records.Size = new System.Drawing.Size(248, 35);
+            this.Records.TabIndex = 5;
             this.Records.Text = "Records";
             this.Records.UseVisualStyleBackColor = false;
             this.Records.Click += new System.EventHandler(this.Records_Click);
@@ -86,8 +86,8 @@
             this.Instrucciones.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Instrucciones.Location = new System.Drawing.Point(21, 62);
             this.Instrucciones.Name = "Instrucciones";
-            this.Instrucciones.Size = new System.Drawing.Size(115, 76);
-            this.Instrucciones.TabIndex = 4;
+            this.Instrucciones.Size = new System.Drawing.Size(248, 35);
+            this.Instrucciones.TabIndex = 0;
             this.Instrucciones.Text = "Instrucciones";
             this.Instrucciones.UseVisualStyleBackColor = false;
             this.Instrucciones.Click += new System.EventHandler(this.Instrucciones_Click);
@@ -98,17 +98,17 @@
             this.LinkRepositorio.Location = new System.Drawing.Point(111, 251);
             this.LinkRepositorio.Name = "LinkRepositorio";
             this.LinkRepositorio.Size = new System.Drawing.Size(73, 13);
-            this.LinkRepositorio.TabIndex = 5;
+            this.LinkRepositorio.TabIndex = 6;
             this.LinkRepositorio.TabStop = true;
             this.LinkRepositorio.Text = "Código fuente";
             this.LinkRepositorio.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkRepositorio_LinkClicked);
             // 
             // Multiplicar
             // 
-            this.Multiplicar.Location = new System.Drawing.Point(154, 149);
+            this.Multiplicar.Location = new System.Drawing.Point(21, 144);
             this.Multiplicar.Name = "Multiplicar";
             this.Multiplicar.Size = new System.Drawing.Size(115, 37);
-            this.Multiplicar.TabIndex = 6;
+            this.Multiplicar.TabIndex = 3;
             this.Multiplicar.Text = "Multiplicar";
             this.Multiplicar.UseVisualStyleBackColor = true;
             this.Multiplicar.Click += new System.EventHandler(this.Multiplicar_Click);
@@ -130,7 +130,7 @@
             this.PlayerNameTextBox.MaxLength = 10;
             this.PlayerNameTextBox.Name = "PlayerNameTextBox";
             this.PlayerNameTextBox.Size = new System.Drawing.Size(114, 22);
-            this.PlayerNameTextBox.TabIndex = 8;
+            this.PlayerNameTextBox.TabIndex = 7;
             this.PlayerNameTextBox.Text = "Jugador";
             this.PlayerNameTextBox.TextChanged += new System.EventHandler(this.PlayerNameTextBox_TextChanged);
             // 
@@ -141,7 +141,7 @@
             this.AclaracioRecordLabel.Location = new System.Drawing.Point(18, 34);
             this.AclaracioRecordLabel.Name = "AclaracioRecordLabel";
             this.AclaracioRecordLabel.Size = new System.Drawing.Size(216, 13);
-            this.AclaracioRecordLabel.TabIndex = 9;
+            this.AclaracioRecordLabel.TabIndex = 7;
             this.AclaracioRecordLabel.Text = "(Tus records se guardaran con este nombre)";
             // 
             // Menu
